b15442766cdeff98ea08351934d9235655bcb0cb
Jit Yong <jityong1997@hotmail.com>
2019-07-08 16:03:59 +0800

    added tabs to filteredpage

--- src/FilteredResult.js
+++ src/FilteredResult.js
@@ -1,10 +1,11 @@
 import React from "react";
-import data from "./chas.json";
+import GP from "./chas.json";
 import * as turf from "@turf/turf";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
+import ResultTabs from "./ResultTabs.js";
+import PC from "./polyclinics.json"
 
 const API_KEY = "AIzaSyDsbjEhJ1510KaVtIQJVTIU7at6hiA__6U";
+
 
 
 // this component aims to display the filtered clinic after they fill in the form
@@ -20,7 +21,8 @@ class FilteredResult extends React.Component {
     this.state = {
       formData: this.props.location.state, //this gets the info from react router from Form.js
       userLng: 0,
-      userLat: 0
+      userLat: 0,
+      filterDist: true
     };
   }
   componentDidMount() {
@@ -39,11 +41,18 @@ class FilteredResult extends React.Component {
   }
   render(props) {
     const { userLat, userLng, formData } = this.state;
-    console.log(userLng, userLat);
+    // polyclinics.clinics.map(clinic=> {
+    //   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${
+    //     clinic.PostalCode
+    //   }&region=sg&key=${API_KEY}`)
+    //   .then(res => res.json())
+    //   .then(json => console.log(clinic.Name,json.results[0].geometry.location))
+        
+    // })
     // console.log("test");
     //below is to filter clinics that are "<= 1" km away from input postal code
     //using the turf distance api <-- can google for more info
-    const filteredClinics = data.features.filter(clinic => {
+    const filteredGP = GP.features.filter(clinic => {
       const from = turf.point([userLng, userLat]);
       const to = turf.point([
         clinic.geometry.coordinates[0],
@@ -57,6 +66,19 @@ class FilteredResult extends React.Component {
       }
       return dist <= 1;
     });
+
+    const filteredPC = PC.clinics.filter(clinic => {
+      const from = turf.point([userLng, userLat]);
+      const to = turf.point([
+        clinic.coord[0],
+        clinic.coord[1]
+      ]);
+      const options = { units: "kilometers" };
+      const dist =  turf.distance(from, to, options);
+      clinic.distance = dist;
+      return dist <= 100;
+    });
+
     function sortDist (a,b) {
       if (a.distance < b.distance){
         return -1;
@@ -64,40 +86,24 @@ class FilteredResult extends React.Component {
         return 1;
       }
     }
-    const sortedClinics = filteredClinics.sort(sortDist);
+    const sortedGP = filteredGP.sort(sortDist);
+    const sortedPC = filteredPC.sort(sortDist);
     //note: dangerouslySetInnerHTML cos the json is in string, but its actually HTML
     return (
       <div>
         <h1>Filtered clinics for S{formData.postalCode}</h1>
         <h1>Subsidies: {formData.subsidyType === "" ? "None" : formData.subsidyType}</h1>
         <div>
-          {sortedClinics.map(clinic=> {
+        <ResultTabs GP={sortedGP} PC={sortedPC}/>
+          {/* {sortedClinics.map(clinic=> {
             return (
             <div> 
-              {clinic.properties.HCI_NAME}  {clinic.properties.CLINIC_PROGRAMME_CODE}
+              {clinic.properties.HCI_NAME}  
               <hr/>
             </div>
           )
-          })}
+          })} */}
         </div>
-        {/* <div className="container">
-          <div className="row">
-            {filteredClinics.map(clinic => {
-              return (
-                <div className="col-md-3 border border-secondary"
-                  style={{margin: "10px"}}>
-                <div 
-                  key={clinic.properties.Name}
-                  dangerouslySetInnerHTML={{
-                    __html: clinic.properties.Description
-                  }}
-                />
-                <button style={{display:"table-cell",verticalAlign:"bottom"}}type="button" class="btn btn-success"> Compare </button>
-                </div>
-              );
-            })}
-          </div>
-        </div> */}
       </div>
     );
   }
