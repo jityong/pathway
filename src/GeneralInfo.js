ba57540499086b77de337cda807f236db70fb4db
Jit Yong <jityong1997@hotmail.com>
2019-08-14 00:40:08 +0800

    updates

--- src/GeneralInfo.js
+++ src/GeneralInfo.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-
+import infographics from "./infographics.png"
 //haven't included any info here, can do styling for RWD(responsive web design)
 function GeneralInfo() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>General Info</h1>
-
-      <p style={{textAlign: "center"}}>INFO on GP vs Polyclinic</p>
+      <h1 style={{ textAlign: "center", fontStyle:"italic" }}>General Info</h1>
+      <Fragment>
+      <img src={infographics} alt="infographics" style={{width:"100%"}}></img>
+      </Fragment>
       <button style={{ float: "right" }}>
         <Link to="/Form">Ok, Next</Link>
       </button>
