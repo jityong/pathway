23d414e16bd8c04a8feade63bb2d96494c96037b
Jit Yong <jityong1997@hotmail.com>
2019-09-04 21:11:26 +0800

    changed language page and home page

--- src/Language.js
+++ src/Language.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./App.css";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,55 +7,101 @@ import ArrowBack from "@material-ui/icons/ArrowBackIos";
 import HelpOutline from "@material-ui/icons/HelpOutline";
 import ArrowNext from "@material-ui/icons/NavigateNext";
 import { Typography, Button, Card } from "@material-ui/core";
+import Background from "./BackgroundFaded.png";
 
-
-
-//Choose langauge, but all buttons go to same place anyway. Could use some styling
+//Choose langauge, but all Buttons go to same place anyway. Could use some styling
 
 function Language() {
-
-  function handleAlert(){
-    alert("Only English language is available at this stage.")
+  function handleAlert() {
+    alert("Only English language is available currently.");
   }
   return (
-    <body className="languagePage">
-      <div>
-        <p style={{fontSize:"15vw", font:"Merriweather"}}> LANGUAGE </p>
-        <div>
-          <button style={{ backgroundColor: "#FF6565", borderRadius: "15px" }}>
-            <Link to="/GeneralInfo">
-              <span style={{ fontSize: "10vw", color: "black" }}>English</span>
-            </Link>
-          </button>{" "}
-        </div>
-        <br />
-        <div>
-          <button style={{ backgroundColor: "#FD9535", borderRadius: "15px" }} onClick={handleAlert}>
-            <Link to="/GeneralInfo">
-              <span style={{ fontSize: "10vw", color: "black" }}>中文</span>
-            </Link>
-          </button>{" "}
-        </div>
-        <br />
-        <div>
-          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }} onClick={handleAlert}>
-            <Link to="/GeneralInfo">
-              <span style={{ fontSize: "10vw", color: "black" }}>
-                Bahasa Melayu
-              </span>
-            </Link>
-          </button>{" "}
-        </div>
-        <br />
-        <div>
-          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }} onClick={handleAlert}>
-            <Link to="/GeneralInfo">
-              <span style={{ fontSize: "10vw", color: "black" }}>Tamil</span>
-            </Link>
-          </button>{" "}
-        </div>
+    <div
+      style={{
+        minHeight:"100vh",
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+       
+      }}
+    >
+      <AppBar position="static" style={{ backgroundColor: "#ff7c01" }}>
+        <Toolbar>
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <ArrowBack />
+              <Typography variant="subtitle1">Back</Typography>
+            </IconButton>{" "}
+          </Link>
+          <Typography variant="h5" align="center" style={{ flexGrow: 1 }}>
+            LANGUAGE
+          </Typography>
+          <Link to="/Form" style={{ textDecoration: "none", color: "white" }}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleAlert}
+            >
+              <Typography variant="subtitle1" /> <HelpOutline />
+            </IconButton>{" "}
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <br />
+      <br />
+      <div style={{ textAlign: "center"}} >
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "#FF6565", borderRadius: "15px" }}
+        >
+          <Link to="/GeneralInfo">
+            <span style={{ fontSize: "10vw", color: "black" }}>English</span>
+          </Link>
+        </Button>{" "}
       </div>
-    </body>
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="default"
+          style={{ borderRadius: "15px" }}
+          onClick={handleAlert}
+        >
+          <Link to="/GeneralInfo">
+            <span style={{ fontSize: "10vw", color: "black" }}>中文</span>
+          </Link>
+        </Button>{" "}
+      </div>
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="default"
+          style={{ borderRadius: "15px" }}
+          onClick={handleAlert}
+        >
+          <Link to="/GeneralInfo">
+            <span style={{ fontSize: "10vw", color: "black" }}>
+              Bahasa Melayu
+            </span>
+          </Link>
+        </Button>{" "}
+      </div>
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="default"
+          style={{ borderRadius: "15px" }}
+          onClick={handleAlert}
+        >
+          <Link to="/GeneralInfo">
+            <span style={{ fontSize: "10vw", color: "black" }}>Tamil</span>
+          </Link>
+        </Button>{" "}
+      </div>
+    </div>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -3,63 +3,30 @@ import { Link } from "react-router-dom";
 import "./App.css";
 import Fancyline from "./Fancylines";
 import lock from "./lock.png";
-
+import { Typography, Button, Card, Grid } from "@material-ui/core";
+import Background from "./PathwayBG.png";
 //Login page, which is the first page user will see. Need to change background
 //color to blue (whole page & considering responsive web design) for it to look good
 
-
 function Login() {
-  return ( 
-
-        <div style={{backgroundColor: "#4d92fb",
-          height: "100%",
-          width: "100%",
-          minHeight: "100vh",
-          minWidth: "100vw"
-         }}>
-          <p className="Title"> Pathway</p>
-        <Fancyline />
-        <p
-          style={{
-            textAlign: "center",
-            fontFamily: "Lobster",
-            fontSize: "6vw",
-            minWidth: "200px",
-            color: "white",
-            // backgroundColor: "#4D92FB",
-            display:"block"
-          }}
-        >
-          Make an informed decision on
-          <br />
-          your choice of primary-care provider
-        </p>
-        <div
-          style={{
-            // backgroundColor: "#4D92FB",
-            textAlign: "center",
-            display:"block"
-            // margin: "10em"
-          }}
-        >
-          <button className="btn btn-danger">
-            <Link to="/Language">
-              <label
-                style={{
-                  textAlign: "center",
-                  font: "Roboto Condensed",
-                  fontSize: "10vm",
-                  color: "white"
-                }}
-              >
-                {/* SINGPASS LOGIN
-                <img src={lock} alt="lock" height="50vw" width="50vw" />  */}
-                CONTINUE
-              </label>
-            </Link>
-          </button>
-        </div>
-        </div>
+  return (
+    <div
+      style={{
+        textAlign:"center",
+        minHeight: "100vh",
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover"
+      }}
+    >
+    <div style={{display:"flex" ,alignItems:"flexEnd"}}>
+      <Link to="/Language">
+        <Button variant="contained" color="secondary" size="large">
+          CONTINUE
+        </Button>
+      </Link>
+    </div>
+    </div>
   );
 }
 
