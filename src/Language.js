5e8faa58cb587d6a996f7b7bd4a83c4410dc41d9
Jit Yong <jityong1997@hotmail.com>
2019-09-04 16:44:46 +0800

    language page alert update

--- src/Language.js
+++ src/Language.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBack from "@material-ui/icons/ArrowBackIos";
+import HelpOutline from "@material-ui/icons/HelpOutline";
+import ArrowNext from "@material-ui/icons/NavigateNext";
+import { Typography, Button, Card } from "@material-ui/core";
+
+
 
 //Choose langauge, but all buttons go to same place anyway. Could use some styling
 
 function Language() {
+
+  function handleAlert(){
+    alert("Only English language is available at this stage.")
+  }
   return (
     <body className="languagePage">
       <div>
@@ -18,7 +31,7 @@ function Language() {
         </div>
         <br />
         <div>
-          <button style={{ backgroundColor: "#FD9535", borderRadius: "15px" }}>
+          <button style={{ backgroundColor: "#FD9535", borderRadius: "15px" }} onClick={handleAlert}>
             <Link to="/GeneralInfo">
               <span style={{ fontSize: "10vw", color: "black" }}>中文</span>
             </Link>
@@ -26,7 +39,7 @@ function Language() {
         </div>
         <br />
         <div>
-          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }}>
+          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }} onClick={handleAlert}>
             <Link to="/GeneralInfo">
               <span style={{ fontSize: "10vw", color: "black" }}>
                 Bahasa Melayu
@@ -36,7 +49,7 @@ function Language() {
         </div>
         <br />
         <div>
-          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }}>
+          <button style={{ backgroundColor: "#9CE939", borderRadius: "15px" }} onClick={handleAlert}>
             <Link to="/GeneralInfo">
               <span style={{ fontSize: "10vw", color: "black" }}>Tamil</span>
             </Link>
