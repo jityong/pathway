651aea940290f3ced77dd8bdfe046aa8bccf6efc
Isaac Quek <isaacquek97@gmail.com>
2019-09-05 16:45:23 +0800

    Varen's login page

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,9 @@ import "./App.css";
 import Fancyline from "./Fancylines";
 import lock from "./lock.png";
 import { Typography, Button, Card, Grid } from "@material-ui/core";
-import Background from "./PathwayBG.png";
+//import Background from "./PathwayBG.png";
+import homepage from "./homepage.png";
+
 //Login page, which is the first page user will see. Need to change background
 //color to blue (whole page & considering responsive web design) for it to look good
 
@@ -13,11 +15,10 @@ function Login() {
     <div
       style={{
         minHeight: "100vh",
-        backgroundImage: `url(${Background})`,
+        backgroundImage: `url(${homepage})`,
         backgroundPosition: "center",
         backgroundSize: "cover"
-      }}
-    >
+      }}>
     <div style={{textAlign:"center",verticalAlign:"bottom",bottom:"0px" }}>
       <Link to="/Language">
         <Button variant="contained" color="secondary" size="large">
