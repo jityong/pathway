ec00298390aafc1988252f79dbb22a76079589a0
Jit Yong <jityong1997@hotmail.com>
2020-01-08 18:06:49 +0800

    added google analytics to track traffic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React from "react";
 import Login from "./pages/Welcome";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Language from "./pages/Language";
 import GeneralInfo from "./pages/GeneralInfo";
 import Form from "./pages/PatientForm";
 import FilteredResult from "./pages/FilteredResult";
 import ConfirmClinicChoicePage from "./pages/ConfirmClinicChoice";
 import confirmedChoicePage from "./pages/SummaryPage";
-
+import GA from './util/GoogleAnalytics'
 
 
 //here is where i put all the routes tgt. can include the help button here which will
 //then be present in all the subsequent pages
 
 class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/Language" exact component={Language} />
-            <Route path="/GeneralInfo" exact component={GeneralInfo} />
-            <Route path="/Form" exact component={Form} />
-            <Route path="/FilteredResult" exact component={FilteredResult} />
-            <Route path="/ConfirmClinicChoice" exact component={ConfirmClinicChoicePage} />
-            <Route path="/confirmedChoice" exact component={confirmedChoicePage} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                {GA.init() && <GA.RouteTracker/>}
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Login}/>
+                        <Route path="/Language" exact component={Language}/>
+                        <Route path="/GeneralInfo" exact component={GeneralInfo}/>
+                        <Route path="/Form" exact component={Form}/>
+                        <Route path="/FilteredResult" exact component={FilteredResult}/>
+                        <Route path="/ConfirmClinicChoice" exact component={ConfirmClinicChoicePage}/>
+                        <Route path="/confirmedChoice" exact component={confirmedChoicePage}/>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
+
 export default App;
