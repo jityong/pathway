dd14b669ab9779836a5fb953d540083a1fe2380a
Jit Yong <jityong1997@hotmail.com>
2020-02-12 23:42:44 +0800

    test heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// API calls
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
