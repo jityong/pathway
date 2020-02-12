89cc9b1bd0e021180fb4dba409f626e380ee0b90
Jit Yong <jityong1997@hotmail.com>
2020-02-12 22:32:53 +0800

    test heroku hosting

---
+++ build/precache-manifest.5cbbcc557586ad77fa671025b81e5c3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719f2c5b83107f1ce579990a0ba1e2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff07385db6731c6688a",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "decdf7c3f439137cb474",
+    "url": "/static/css/main.51e66f74.chunk.css"
+  },
+  {
+    "revision": "aff07385db6731c6688a",
+    "url": "/static/js/2.416bd689.chunk.js"
+  },
+  {
+    "revision": "decdf7c3f439137cb474",
+    "url": "/static/js/main.6271e8b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38bffda3d34547db88b4d1e71f5e9407",
+    "url": "/static/media/BackgroundFaded.38bffda3.png"
+  },
+  {
+    "revision": "09d0b79e775bdfaa2e04f873c6052f99",
+    "url": "/static/media/homepageLogo.09d0b79e.png"
+  },
+  {
+    "revision": "72b3f2355bc59bc1730a20fdfeb98ba1",
+    "url": "/static/media/infographics.72b3f235.png"
+  }
+]);
