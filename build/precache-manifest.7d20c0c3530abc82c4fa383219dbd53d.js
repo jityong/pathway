59e8e2ef760cdf182188f62eefe2f1a95a0d693d
Leow jit yong <jityong@jityong.lan>
2020-06-05 12:37:03 +0800

    adding build

---
+++ build/precache-manifest.7d20c0c3530abc82c4fa383219dbd53d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f898d7d29be1e2ec8b0fa08826b45b5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6293e91d327046a01bed",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "8e2734aacc237fb7bd29",
+    "url": "/static/css/main.774d9563.chunk.css"
+  },
+  {
+    "revision": "6293e91d327046a01bed",
+    "url": "/static/js/2.5b0a4d97.chunk.js"
+  },
+  {
+    "revision": "8e2734aacc237fb7bd29",
+    "url": "/static/js/main.377aebf0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38bffda3d34547db88b4d1e71f5e9407",
+    "url": "/static/media/BackgroundFaded.38bffda3.png"
+  },
+  {
+    "revision": "09d0b79e775bdfaa2e04f873c6052f99",
+    "url": "/static/media/homepageLogo.09d0b79e.png"
+  },
+  {
+    "revision": "72b3f2355bc59bc1730a20fdfeb98ba1",
+    "url": "/static/media/infographics.72b3f235.png"
+  }
+]);
