eed2478a43d020ccbb50e1fbb0e7b963cf8dd089
jityong <jityong1997@hotmail.com>
2020-06-26 15:41:25 +0800

    updated cors config

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,12 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 const port = process.env.PORT || 5000;
 app.listen(port);
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://pathwaynuhs.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
